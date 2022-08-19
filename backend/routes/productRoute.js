eb2b69c0008bfb8cf6e3ddff69a37e8709b61d79
Arman Ahmed <armanahmedjony786@gmail.com>
2022-08-20 01:32:57 +0600

    added is authenticated middleware in product post route

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -10,8 +10,8 @@ const { isAuthenticated } = require("../middleware/auth");
 
 const router = express.Router();
 
-router.route("/products").get(isAuthenticated, getAllProducts);
-router.route("/products").post(createProduct);
+router.route("/products").get(getAllProducts);
+router.route("/products").post(isAuthenticated, createProduct);
 router
   .route("/products/:id")
   .put(updateProduct)
