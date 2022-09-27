9ddc091f5a4cb3907b3970044156d8889b15adc0
Arman Ahmed <armanahmedjony786@gmail.com>
2022-09-28 02:54:18 +0600

    added get all orders of my self api

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -27,6 +27,13 @@ exports.createOrder = catchAsyncFunction(async (req, res) => {
     .status(201)
     .json({ success: 'true', message: 'ordered successfully', order })
 })
+
+//get all my orders --user
+exports.getMyOrders = catchAsyncFunction(async (req, res) => {
+  const orders = await Order.find({ createdBy: req.user.id })
+  res.status(200).json({ success: true, orders })
+})
+
 // get all orders --ADMIN--
 exports.getAllOrders = catchAsyncFunction(async (req, res) => {
   const orders = await Order.find().populate(
--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -3,12 +3,13 @@ const {
   createOrder,
   getSingleOrder,
   getAllOrders,
+  getMyOrders,
 } = require('../controllers/orderController')
 const { isAuthenticated, authorizeRoles } = require('../middleware/auth')
 const router = express.Router()
 
 router.route('/orders').post(isAuthenticated, createOrder)
-
+router.route('/orders').get(isAuthenticated, getMyOrders)
 router
   .route('/admin/orders')
   .get(isAuthenticated, authorizeRoles('admin'), getAllOrders)
