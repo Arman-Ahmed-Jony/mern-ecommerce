0651be479fccea1bea3a8ffef10cc767279cf665
Arman Ahmed <armanahmedjony786@gmail.com>
2022-10-11 00:03:40 +0600

    added footer section

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,10 @@
 import './App.css'
-import Header from './components/layout/Header.js'
-import { BrowserRouter as Router } from 'react-router-dom'
+import Header from './components/layout/Header/Header.js'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import { useEffect } from 'react'
 import WebFont from 'webfontloader'
+import Footer from './components/layout/Footer/Footer'
+import Home from './components/Home/Home'
 function App() {
   useEffect(() => {
     WebFont.load({
@@ -10,10 +12,15 @@ function App() {
         families: ['Roboto', 'Droid Sens', 'Chilanka'],
       },
     })
-  })
+  }, [])
   return (
     <Router>
       <Header />
+      <div style={{ marginTop: '120px' }}></div>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+      </Routes>
+      <Footer />
     </Router>
   )
 }
