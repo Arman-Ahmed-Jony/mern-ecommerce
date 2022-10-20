fbc8791ad3bff9888890e64916423023998142d9
Arman Ahmed <armanahmedjony786@gmail.com>
2022-10-21 00:19:48 +0600

    added product compoennt

--- frontend/src/modules/Products/components/Product.jsx
+++ frontend/src/modules/Products/components/Product.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 function Product({ img, title, description }) {
   return (
-    <>
-      <img src={img.src} alt="product images" width="500px" />
+    <div style={{ width: '400px', margin: '10px' }}>
+      <img src={img.src} alt="product images" width="400px" />
       <p>{title}</p>
       <p>{description}</p>
-    </>
+    </div>
   )
 }
 
--- frontend/src/modules/Products/index.jsx
+++ frontend/src/modules/Products/index.jsx
@@ -1,18 +1,79 @@
 import React from 'react'
+import { useEffect } from 'react'
+import { useState } from 'react'
 import Product from './components/Product'
 
 function Products() {
+  const [products, setProducts] = useState([])
+  const getProducts = () => {
+    setProducts([
+      {
+        title: 'product 1',
+        description: 'product description',
+        src: 'https://media.istockphoto.com/photos/background-for-cosmetic-products-of-natural-beige-color-picture-id1273934874?s=612x612',
+      },
+      {
+        title: 'product 2',
+        description: 'product description',
+        src: 'https://media.istockphoto.com/photos/background-for-cosmetic-products-of-natural-beige-color-picture-id1273934874?s=612x612',
+      },
+      {
+        title: 'product 3',
+        description: 'product description',
+        src: 'https://media.istockphoto.com/photos/background-for-cosmetic-products-of-natural-beige-color-picture-id1273934874?s=612x612',
+      },
+      {
+        title: 'product 1',
+        description: 'product description',
+        src: 'https://media.istockphoto.com/photos/background-for-cosmetic-products-of-natural-beige-color-picture-id1273934874?s=612x612',
+      },
+      {
+        title: 'product 2',
+        description: 'product description',
+        src: 'https://media.istockphoto.com/photos/background-for-cosmetic-products-of-natural-beige-color-picture-id1273934874?s=612x612',
+      },
+      {
+        title: 'product 3',
+        description: 'product description',
+        src: 'https://media.istockphoto.com/photos/background-for-cosmetic-products-of-natural-beige-color-picture-id1273934874?s=612x612',
+      },
+      {
+        title: 'product 1',
+        description: 'product description',
+        src: 'https://media.istockphoto.com/photos/background-for-cosmetic-products-of-natural-beige-color-picture-id1273934874?s=612x612',
+      },
+      {
+        title: 'product 2',
+        description: 'product description',
+        src: 'https://media.istockphoto.com/photos/background-for-cosmetic-products-of-natural-beige-color-picture-id1273934874?s=612x612',
+      },
+      {
+        title: 'product 3',
+        description: 'product description',
+        src: 'https://media.istockphoto.com/photos/background-for-cosmetic-products-of-natural-beige-color-picture-id1273934874?s=612x612',
+      },
+    ])
+  }
+
+  useEffect(() => {
+    getProducts()
+  }, [])
   return (
-    <>
-      <Product
-        title="test product"
-        description="test description"
-        img={{
-          src: 'https://media.istockphoto.com/photos/background-for-cosmetic-products-of-natural-beige-color-picture-id1273934874?s=612x612',
-          alt: 'product image for cosmetic products of natural',
-        }}
-      />
-    </>
+    <div
+      style={{
+        display: 'flex',
+        flexWrap: 'wrap',
+        justifyContent: 'space-around',
+      }}
+    >
+      {products.map((product) => (
+        <Product
+          title={product.title}
+          description={product.description}
+          img={{ src: product.src }}
+        />
+      ))}
+    </div>
   )
 }
 
