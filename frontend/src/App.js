7086bed4c42ed633b27ab61ccd941bdc7cff7e8c
arman ahmed <armanahmedjony786@gmail.com>
2023-07-13 06:58:15 +0600

    enable webfont

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,18 +2,18 @@ import './App.css'
 import Header from './layout/Header/Header.js'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import { useEffect } from 'react'
-// import WebFont from 'webfontloader'
+import WebFont from 'webfontloader'
 import Footer from './layout/Footer/Footer'
 import Home from './modules/Home'
 import ProductDetails from './modules/Products/ProductDetailsPage'
 import LoginSignUp from './modules/User/LoginSignUp'
 function App() {
   useEffect(() => {
-    // WebFont.load({
-    //   google: {
-    //     families: ['Roboto', 'Droid Sens', 'Chilanka'],
-    //   },
-    // })
+    WebFont.load({
+      google: {
+        families: ['Roboto', 'Droid Sens', 'Chilanka'],
+      },
+    })
   }, [])
   return (
     <Router>
